71c41f206b2ea43854e23e8d8bca3d4a3d4a7487
Barry Gordon <hello@barrygordon.co.uk>
2014-01-06 23:55:08 +0000

    Rough implementation of a post-spec summary

--- lib/factory_inspector.rb
+++ lib/factory_inspector.rb
@@ -23,6 +23,7 @@ module FactoryInspector
     report_file = output_filename || Configuration.default_report_path
     self.inspector.generate_report(report_file)
     puts "\nFactory Inspector report in '#{report_file}'"
+    generate_summary
   end
 
   class Inspector
@@ -36,32 +37,20 @@ module FactoryInspector
       @inspection_start_time = Time.now
     end
 
-    def generate_report(output_filename)
-      inspection_time_in_seconds = Time.now - @inspection_start_time
-      factory_time_in_seconds = calculate_total_factory_time_in_seconds
-      percentage_time_in_factories = ( (factory_time_in_seconds * 100) / inspection_time_in_seconds )
-
-      file = File.open(output_filename, 'w')
-
-      file.write "FACTORY INSPECTOR\n"
-      file.write "  - #{@reports.values.size} factories used, #{calculate_total_factory_calls} calls made\n"
-      file.write "  - #{sprintf("%6.4f",inspection_time_in_seconds)} seconds of testing inspected\n"
-      #file.write "  - #{sprintf("%6.4f",factory_time_in_seconds)} seconds in factories\n"
-      #file.write "  - #{sprintf("%4.1f",percentage_time_in_factories)}% testing time is factory calls\n"
-      file.write "\n"
-      file.write "  FACTORY NAME                     TOTAL  OVERALL   TIME PER  LONGEST   STRATEGIES\n"
-      file.write "                                   CALLS  TIME (s)  CALL (s)  CALL (s)            \n"
-      @reports.sort_by{ |name,report| report.time_per_call_in_seconds }.reverse.each do |report_name, report|
-        line = sprintf("  %-30.30s % 5.0d    %8.4f    %5.5f  %5.4f      %s\n",
-                     report.factory_name,
-                     report.calls,
-                     report.total_time_in_seconds,
-                     report.time_per_call_in_seconds,
-                     report.worst_time_in_seconds,
-                     report.strategies)
-        file.write(line)
+    def generate_summary
+      puts "\n"
+      puts print_header
+      sorted_reports.take(10) do |report_name, report|
+        puts print_formatted_report(report)
       end
+    end
 
+    def generate_report(output_filename)
+      file = File.open(output_filename, 'w')
+      file.write print_header
+      sorted_reports.each do |report_name, report|
+        file.write print_formatted_report(report)
+      end
       file.close
     end
 
@@ -83,13 +72,47 @@ module FactoryInspector
 
   private
 
+    def sorted_reports
+      @reports.sort_by{ |name,report| report.time_per_call_in_seconds }.reverse
+    end
+
+    def print_header
+      header = ''
+      header += "FACTORY INSPECTOR\n"
+      header += "  - #{@reports.values.size} factories used, #{calculate_total_factory_calls} calls made\n"
+      header += "  - #{sprintf("%6.4f",inspection_time_in_seconds)} seconds of testing inspected\n"
+      #header += "  - #{sprintf("%6.4f",factory_time_in_seconds)} seconds in factories\n"
+      #header += "  - #{sprintf("%4.1f",percentage_time_in_factories)}% testing time is factory calls\n"
+      header += "\n"
+      header += "  FACTORY NAME                     TOTAL  OVERALL   TIME PER  LONGEST   STRATEGIES\n"
+      header += "                                   CALLS  TIME (s)  CALL (s)  CALL (s)            \n"
+    end
+
+    def print_formatted_report(report)
+      sprintf("  %-30.30s % 5.0d    %8.4f    %5.5f  %5.4f      %s\n",
+        report.factory_name,
+        report.calls,
+        report.total_time_in_seconds,
+        report.time_per_call_in_seconds,
+        report.worst_time_in_seconds,
+        report.strategies)
+    end
+
+    def inspection_time_in_seconds
+      @inspection_time_in_seconds ||= Time.now - @inspection_start_time
+    end
+
+    def percentage_time_in_factories
+      ( (total_factory_time_in_seconds * 100) / inspection_time_in_seconds )
+    end
+
     def instrument_factory_girl
       ActiveSupport::Notifications.subscribe('factory_girl.run_factory') do |name, start_time, finish_time, id, payload|
         analyze(payload[:name], start_time, finish_time, payload[:strategy])
       end
     end
 
-    def calculate_total_factory_time_in_seconds
+    def total_factory_time_in_seconds
       @reports.values.reduce(0) do |total_time_in_seconds, report|
         total_time_in_seconds + report.total_time_in_seconds
       end
